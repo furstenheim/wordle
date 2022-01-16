f95f664992f81cc0c22752dfdc6af11091e5f337
Gabriel <furstenheim@gmail.com>
2022-01-16 19:50:38 +0100

    Parse input

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"regexp"
 	"strings"
 )
 
@@ -18,10 +19,54 @@ type Word string
 
 type ComparisonAggregate [253]int
 
+type SharedInput struct {
+	combinations []Combination
+}
 
 func main () {
-	precomputed := computePrecomputed()
+	// precomputed := computePrecomputed()
+	inputFile, readErr := ioutil.ReadFile("input.txt")
+	if readErr != nil {
+		log.Fatal("Error on input", readErr)
+	}
+	inputsRegex := regexp.MustCompile("Wordle \\d{3} \\d/\\d\n\n((\U0001f7e9|\U0001f7e8|\u2b1b|\u2b1c){5}\n)+")
+	inputs := inputsRegex.FindAllString(string(inputFile), -1)
+	sharedInputs := []SharedInput{}
+	for _, v := range(inputs) {
+		log.Println(string(v))
+		sa := SharedInput{combinations: []Combination{}}
+		split := strings.Split(string(v), "\n")
 
+		for _, l := range(split[2:len(split)-1]) {
+			sa.combinations = append(sa.combinations, CombinationString(l).toCombination())
+		}
+		sharedInputs = append(sharedInputs, sa)
+	}
+	log.Println(sharedInputs)
+}
+
+func (c CombinationString) toCombination () Combination {
+	ca := CombinationArray{}
+	var i = 0
+	var v rune
+	for _, v = range(c) {
+		if v == 11035 {
+			ca[i] = Grey
+		} else if v == 11036 {
+			ca[i] = Grey
+		} else if v == 129000 {
+			ca[i] = Yellow
+		} else if v == 129001 {
+			ca[i] = Green
+		} else {
+			log.Fatal("Unknown color", string(v), v)
+		}
+		i++
+	}
+	if i != 5 {
+		log.Fatal("Wrong length for combination", i)
+	}
+	return ca.toNumber()
 }
 
 func computePrecomputed () Precomputed {
